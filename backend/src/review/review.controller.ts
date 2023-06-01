5a25607ece728967b642d1e9943cc293cd17df2f
MohamedMamluk <mohamedmamlukali@gmail.com>
2023-06-01 12:53:47 +0300

    feat: add createReview method

--- backend/src/review/review.controller.ts
+++ backend/src/review/review.controller.ts
@@ -1,7 +1,33 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  Req,
+  UseGuards,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
+import { JwtStrategy } from 'src/auth/jwt.strategy';
+import { CreateReviewDto } from './dto/create-review.dto';
 import { ReviewService } from './review.service';
 
 @Controller('review')
 export class ReviewController {
   constructor(private readonly reviewService: ReviewService) {}
+  @Post('create')
+  @UsePipes(ValidationPipe)
+  @UseGuards(AuthGuard('jwt'))
+  async createReview(
+    @Body() reviewBody: CreateReviewDto,
+    @Req() request: Request,
+  ) {
+    const fullReviewBody: CreateReviewDto & { user: string } = {
+      ...reviewBody,
+      user: request.user as string,
+    };
+    console.log(request.user);
+    return this.reviewService.createReview(fullReviewBody);
+  }
 }
--- backend/src/review/review.service.ts
+++ backend/src/review/review.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CreateReviewDto } from './dto/create-review.dto';
+import { Review, ReviewDocument } from './schemas/review.schema';
 
 @Injectable()
-export class ReviewService {}
+export class ReviewService {
+  constructor(@InjectModel(Review.name) private reviewModel: Model<Review>) {}
+
+  async createReview(reviewBody: CreateReviewDto): Promise<ReviewDocument> {
+    return this.reviewModel.create(reviewBody);
+  }
+}
