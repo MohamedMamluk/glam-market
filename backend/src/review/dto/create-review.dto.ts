9e8766ef6ce7bc08b5c8f30ab7c662ab941400fb
MohamedMamluk <mohamedmamlukali@gmail.com>
2023-06-01 12:53:07 +0300

    feat: create review dto

---
+++ backend/src/review/dto/create-review.dto.ts
@@ -0,0 +1,14 @@
+import { IsNumber, IsMongoId, IsString } from 'class-validator';
+import { Product } from 'src/product/schemas/product.schema';
+import { User } from 'src/user/schemas/user.schema';
+
+export class CreateReviewDto {
+  @IsNumber()
+  rating: number;
+
+  @IsMongoId()
+  product: Product;
+
+  @IsString()
+  comment: string;
+}
