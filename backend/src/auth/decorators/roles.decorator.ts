af015b1f7082ac496f4430f2341e3cf62ad68396
MohamedMamluk <mohamedmamlukali@gmail.com>
2023-07-10 13:12:40 +0300

    feat: add userRoles enum to the roles decorator

--- backend/src/auth/decorators/roles.decorator.ts
+++ backend/src/auth/decorators/roles.decorator.ts
@@ -1,3 +1,4 @@
 import { SetMetadata } from '@nestjs/common';
+import { UserRole } from 'src/user/schemas/user.schema';
 
-export const Roles = (...role: string[]) => SetMetadata('role', role);
+export const Roles = (...role: UserRole[]) => SetMetadata('roles', role);
