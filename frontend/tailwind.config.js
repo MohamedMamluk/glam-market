51b044179e8d20136ad1f04cfe0c9befb779ce84
MohamedMamluk <mohamedmamlukali@gmail.com>
2023-08-04 16:39:02 +0300

    feat: switch to nextjs

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,21 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,ts}"],
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
   theme: {
     extend: {
-      colors: {
-        Pink: "#FB2E86",
-        Red: "#EB5757",
-        Blue: "#2F1AC4",
-        OffBlue: "#151875",
-        NavyBlue: "#151875",
-        OffNavyBlue: "#3F509E",
-        SkyBlue: "#F3F9FF",
-        Purple: "#7E33E0",
-        OffPurple: "#9F63B5",
-        PantonePurple: "#E0D3F5",
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
   plugins: [],
-};
+}
